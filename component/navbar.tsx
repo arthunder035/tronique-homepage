8a5a3d8abd1696e8df013b00a2e81e2e5f06eeb9
sky710212 <sky710212@gmail.com>
2024-05-06 15:20:49 +0530

    Adding tron wallet button

--- component/navbar.tsx
+++ component/navbar.tsx
@@ -1,17 +1,20 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../public/TroniQue (1) 1.svg'
 import '../styles/navbar.css'
 import Link from 'next/link';
+import ConnectWallet from '../src/TronWallet/TronConnect'
 
 
 const Navbar = () => {
+  const [showWalletConnect, setShowWalletConnect] = useState(false);
   return (
     <div className='navbar'>
         <Link href="/">
             <Image src={logo} alt='logo'></Image>
         </Link>
-      <button className='wallet-button'>Connect Wallet</button>
+      <ConnectWallet/>
     </div>
   )
 }
